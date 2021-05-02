cf7c3e3a9b20e973df6224b8e4c8587e4e9dfb51
40013393 <sriram.rm1@ltts.com>
2021-05-02 18:02:52 +0530

    imported sum.py and used it

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import sum
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("Thats all")
+    print(sum.summ(2,3))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
